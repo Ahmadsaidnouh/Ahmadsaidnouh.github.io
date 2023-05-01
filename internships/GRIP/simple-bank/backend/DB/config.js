f297937f111afedf739f43cb2d848cf2b27de978
Ahmadsaidnouh <es-ahmad.nouh2025@alexu.edu.eg>
2023-05-01 07:56:16 +0300

    delete simple-bank-backend

--- internships/GRIP/simple-bank/backend/DB/config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const initConnection = () => {
-    return mongoose.connect(process.env.CONNECTION_STRING)
-        .then(() => console.log("connected"))
-        .catch((error) => console.log(error));
-}
-
-module.exports = initConnection;
--- internships/GRIP/simple-bank/backend/routes/allroutes.js
+++
@@ -1,4 +0,0 @@
-const userRoutes = require("../modules/user/user.routes")
-const transferRoutes = require("../modules/transfer/transfer.routes");
-
-module.exports = {userRoutes, transferRoutes };
